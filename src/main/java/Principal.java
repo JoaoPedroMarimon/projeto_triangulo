8b936dbaa5353a48f8a23f4bf411a78849579574
10726112374 <10726112374@UNSO7DT98909.animaedu.intranet>
2026-03-17 21:01:40 -0300

    innit: primeiro commit

---
+++ src/main/java/Principal.java
@@ -0,0 +1,19 @@
+
+
+
+
+/**
+ *
+ * @author 10726112374
+ */
+import javax.swing.JOptionPane;
+public class Principal {
+
+    public static void main(String[] args) {
+        double base, altura, area;
+        base = Double.parseDouble(JOptionPane.showInputDialog("Digite a base"));
+        altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura"));
+        area = (base * altura)/2.0;
+        JOptionPane.showMessageDialog(null,"A area é " + area);
+    }
+}
